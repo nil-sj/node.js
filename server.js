fa04e339fdcfdcae340fbe694bb4dd87b5eaf358
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 08:18:24 -0500

    Example of using package.json - creating a basic server

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 const http = require("http");
-const path = require("path");
-const fs = require("fs");
+
+const hostname = 'localhost';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
     console.log("Requested URL:", req.url);
     console.log(req.headers);
 
-    // Set the default file to serve
-    let filePath = path.resolve("./public/index.html");
-
-    // Check if the file exists
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.error("File not found!");
-            res.statusCode = 404;
-            res.setHeader("Content-Type", "text/html");
-            res.end("<h1>404 Not Found</h1><p>The requested file was not found on this server.</p>");
-            return;
-        }
-
-        // If accessible, read and serve the file
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-
-        const readStream = fs.createReadStream(filePath);
-        readStream.pipe(res);
-    });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Hello World!</h1></body></html>');
 });
 
-server.listen(3000, "localhost", () => {
-    console.log("Server running at http://localhost:3000/");
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
