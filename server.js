63d656872334c662104b7ef0398ca85fedfdf54d
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 08:20:10 -0500

    Example of a basic server - sending plain text response

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const server = http.createServer((req, res) => {
     console.log(req.headers);
 
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World!</h1></body></html>');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
 });
 
 server.listen(port, hostname, () => {
