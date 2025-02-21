f672fc82da6777b4321920a6fea8829b62ebbfcd
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 07:11:23 -0500

    Creating an HTTP Server using HTTP module

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><head><title>Sample Server Response</title></head><body><h1>Hello, World!</h1><p>(This is an example reponse from the Server)</p></body></html>");
+});
+
+server.listen(3000, "localhost", () => {
+    console.log("Server running at http://localhost:3000/");
+});
