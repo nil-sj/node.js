693f4d0d6ed972b58b8578c518ce42affd9c3c1f
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 07:21:19 -0500

    Example of 'res.write()'

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const server = http.createServer((req, res) => {
     console.log("Requested URL:", req.url);
     console.log(req.headers);
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<html><head><title>Sample Server Response</title></head><body><h1>Hello, World!</h1><p>(This is an example reponse from the Server)</p></body></html>");
+    res.write("<html><head><title>Sample Server Response</title></head><body><h1>Hello, World!</h1><p>(This is an example reponse from the Server)</p></body></html>");
+    res.end();
+    // the above two lines can be combined into a single line:
+    // res.end("<html><head><title>Sample Server Response</title></head><body><h1>Hello, World!</h1><p>(This is an example reponse from the Server)</p></body></html>");
 });
 
 server.listen(3000, "localhost", () => {
