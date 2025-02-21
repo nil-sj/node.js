274bd3dbb3a8d92a4e495ec84f77daa4bb57b9af
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 07:40:57 -0500

    Example of fs and path modules

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     console.log("Requested URL:", req.url);
     console.log(req.headers);
-    res.statusCode = 200;
-    res.write("<html><head><title>Sample Server Response</title></head><body><h1>Hello, World!</h1><p>(This is an example reponse from the Server)</p></body></html>");
-    res.end();
-    // the above two lines can be combined into a single line:
-    // res.end("<html><head><title>Sample Server Response</title></head><body><h1>Hello, World!</h1><p>(This is an example reponse from the Server)</p></body></html>");
+
+    // Set the default file to serve
+    let filePath = path.resolve("./public/index.html");
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error("File not found!");
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "text/html");
+            res.end("<h1>404 Not Found</h1><p>The requested file was not found on this server.</p>");
+            return;
+        }
+
+        // If accessible, read and serve the file
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+
+        const readStream = fs.createReadStream(filePath);
+        readStream.pipe(res);
+    });
 });
 
 server.listen(3000, "localhost", () => {
